a673ffad774b266cb61e1ae6f929806948e8e053
YiowHur_Ng <yiowhur_ng@asus.com>
2023-07-27 17:51:12 +0800

    LTE: Run init script as daemon
    Change-Id: Ia847f9faf6c401dc5b3b11134014deb84b2d5fed
    Signed-off-by: YiowHur_Ng <yiowhur_ng@asus.com>

--- recipes-bsp/lte-init/files/lte-init.sh
+++ recipes-bsp/lte-init/files/lte-init.sh
@@ -5,16 +5,16 @@ PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 case "$1" in
 	start)
 		echo "Starting LTE init"
-		/etc/modem/mm_cli start &
+		start-stop-daemon --start --background --exec /etc/modem/mm_cli  -- start
 		if asus_cmcli keepalive status; then
-			/etc/mm/mm_keepalive run &
+			start-stop-daemon --start --background --exec /etc/mm/mm_keepalive -- run
 		fi
 		echo "."
 		;;
 	stop)
 		echo "Stoping LTE init"
-		/etc/modem/mm_cli stop &
-		/etc/mm/mm_keepalive stop &
+		/etc/modem/mm_cli stop
+		/etc/mm/mm_keepalive stop
 		echo "."
 		;;
 	restart|reload)
